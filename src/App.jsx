5c3fe9ba6a9767fa78973414b8cebc027a824f33
Corina <corinajimenez061102@gmail.com>
2025-11-19 22:10:55 -0300

    Eliminamos archivos innecesarios, limpiamos carpetas y agregamos codigo en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 function App() {
-  
   return (
     <>
-      hola mundo
-        
+      <main className="container">
+        <h1 className="my-5">Ejercicio 1</h1>
+        <h2>Hello Word! 😊</h2>
+      </main>
+      <footer className="container my-3 ">&copy; Corina Jimenez 🎀</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
